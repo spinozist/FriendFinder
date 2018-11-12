3d6ed1579b396a04a00a1918b66c3cd8bbe2905c
Spinozist <ewoodworth@atlantaregional.org>
2018-11-12 16:23:32 -0500

    added alert for the meantime

--- server.js
+++ server.js
@@ -33,31 +33,34 @@ var friends = [
             1,
             1,
             1,
-            5
+            5,
+            5,
+            4,
+            4
         ]
     },
     {
         name: 'Ms. Neutral',
         slackID: 'vanilla',
-        scores: ['3', '3', '3', '3', '3', '3', '3']
+        scores: ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
     },
 
     {
         name: 'Mr. Asshole',
         slackID: 'asshole',
-        scores: ['5', '1', '5', '5', '5', '5', '1']
+        scores: ['5', '1', '5', '5', '5', '5', '1', '1', '1', '1']
     },
 
     {
         name: 'Mrs. Nice',
         slackID: 'nice-lady',
-        scores: ['1', '4', '2', '4', '1', '2', '4']
+        scores: ['1', '4', '2', '4', '1', '2', '4', '2', '2', '1']
     },
 
     {
         name: 'Dr. Whatever',
         slackID: 'whatev-Rx',
-        scores: ['4', '2', '3', '4', '3', '2', '1']
+        scores: ['4', '2', '3', '4', '3', '2', '1', '1', '1', '4']
     }
 ];
 
