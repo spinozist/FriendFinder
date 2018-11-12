dd41672ef9c1e5788131bc598a4ab9a44e20f9fc
Spinozist <ewoodworth@atlantaregional.org>
2018-11-11 19:54:49 -0500

    working api router and survey html

--- server.js
+++ server.js
@@ -11,20 +11,43 @@ app.use(express.json());
 //HTML Routing
 
 //GET ROUTE to `/survey` to display survey.html
-app.get(`/survey`, function(req, res) {
+app.get(`/survey`, function (req, res) {
     res.sendFile(`${__dirname}/app/public/survey.html`);
-  });
+});
 
 //Default to /index.html
-app.get(`/`, function(req, res) {
+app.get(`/`, function (req, res) {
     res.sendFile(`${__dirname}/app/public/home.html`);
+});
+
+var friends = [];
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
   });
 
-  
+app.post("/api/friends", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newFriend
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
+    console.log(newFriend);
+
+    friends.push(newFriend);
+
+    res.json(newFriend);
+});
 
 
 
 
-app.listen(PORT, function() {
+
+
+
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
