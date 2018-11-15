aa6570ca6fe70670da877272203bec80aceafc53
Spinozist <ewoodworth@atlantaregional.org>
2018-11-15 16:50:27 -0500

    added process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require(`express`);
 // var path = require(`path`);
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
