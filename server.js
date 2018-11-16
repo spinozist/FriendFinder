2d31f4e65db664747ca2bcaf9f04dc849641da8e
Spinozist <ewoodworth@atlantaregional.org>
2018-11-15 21:30:55 -0500

    externalized friends.js

--- server.js
+++ server.js
@@ -22,53 +22,7 @@ app.get(`/`, function (req, res) {
 
 //Friends Array
 
-var friends = [
-    {
-        name: "Erik",
-        slackID: "spinozist",
-        scores: ['2', '1', '1', '1', '1', '1', '5', '5', '4', '4']
-    },
-    {
-        name: 'Ms. Neutral',
-        slackID: 'vanilla',
-        scores: ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
-    },
-    {
-        name: 'Mr. Jerk',
-        slackID: 'asshole',
-        scores: ['5', '1', '5', '5', '5', '5', '1', '1', '1', '1']
-    },
-    {
-        name: 'Mrs. Nice',
-        slackID: 'nice-lady',
-        scores: ['1', '4', '2', '4', '1', '2', '4', '2', '2', '1']
-    },
-    {
-        name: 'Dr. Whatever',
-        slackID: 'whatev-Rx',
-        scores: ['4', '2', '3', '4', '3', '2', '1', '1', '1', '4']
-    },
-    {
-        name: 'Trump',
-        slackID: 'trumpster-fire',
-        scores: ['5', '5', '5', '5', '5', '5', '1', '4', '1', '1']
-    },
-    {
-        name: 'Jim',
-        slackID: 'n/a',
-        scores: ['5', '1', '1', '1', '2', '1', '1', '1', '3', '4']
-    },
-    {
-        name: 'David',
-        slackID: 'n/a',
-        scores: ['1', '3', '1', '1', '2', '1', '1', '3', '1', '2']
-    },
-    {
-        name: 'Colby',
-        slackID: 'n/a',
-        scores: ['1', '3', '2', '2', '1', '1', '5', '3', '2', '2']
-    }
-];
+var friends = require(`./app/data/friends.js`);
 
 //
 
